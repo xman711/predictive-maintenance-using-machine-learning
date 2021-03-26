d17cd27d1105a6b8c8aefb2e51a356b230ea192c
xman711 <72877133+xman711@users.noreply.github.com>
2021-03-26 18:39:16 +0800

    Update preprocess.py

--- source/notebooks/sagemaker_predictive_maintenance/preprocess.py
+++ source/notebooks/sagemaker_predictive_maintenance/preprocess.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 columns = ['id', 'cycle', 'setting1', 'setting2', 'setting3', 's1', 's2', 's3','s4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14','s15', 's16', 's17', 's18', 's19', 's20', 's21']
 
+# 前處理 data 資料夾的 test.txt、train.txt 
 def preprocess_data(data_location, columns=columns):
     # normalize sensor readings
     train_df = []
